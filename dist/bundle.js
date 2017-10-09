b7f8806c37b77799f8d7783b4a037352ce84cfdb
zhvirblis <zhvirblis@github.com>
2017-10-09 08:39:20 +0300

    Asset Management: Loading Data

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,6 +81,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__style_css__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__icon_png__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__icon_png___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__icon_png__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__data_xml__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__data_xml___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__data_xml__);
+
 
 
 
@@ -97,6 +100,8 @@ function component() {
     myIcon.src = __WEBPACK_IMPORTED_MODULE_2__icon_png___default.a;
 
     element.appendChild(myIcon);
+
+    console.log(__WEBPACK_IMPORTED_MODULE_3__data_xml___default.a);
 
     return element;
 }
@@ -17856,6 +17861,12 @@ module.exports = function (css) {
 	return fixedCss;
 };
 
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports) {
+
+module.exports = {"note":{"to":["Mary"],"from":["John"],"heading":["Reminder"],"body":["Call Cindy on Tuesday"]}}
 
 /***/ })
 /******/ ]);
