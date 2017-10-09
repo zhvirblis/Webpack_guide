4983f9edb40f6e3872394b1409e996f030a7bb50
zhvirblis <zhvirblis@github.com>
2017-10-09 14:05:28 +0300

    Caching: Output Filenames

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js'
-  },
+  entry: './src/index.js', 
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HTMLWebpackPlugin({
-      title: 'Code Splitting'
+      title: 'Caching'
     })
   ],
   output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   }
 };
