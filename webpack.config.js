9927ccb8e9af0a6e3c4e18d78cfceb79d93ca3e7
zhvirblis <zhvirblis@github.com>
2017-10-09 10:19:25 +0300

    Hot Module Replacement: HMR with Stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
     contentBase: './dist',
     hot: true
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management'
