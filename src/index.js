d6d4d18837a8a39478bb8081c3bb7394b84d98cd
zhvirblis <zhvirblis@github.com>
2017-10-09 10:36:25 +0300

    Tree Shaking: Add a Utility

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    var element = document.createElement('pre');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     return element;
 }
 
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', function(){
-        console.log('Accepting the updated printMe module!');
-        document.body.removeChild(element);
-        element = component(); // Re-render the "component" to update the click handler
-        document.body.appendChild(element);
-    })
-}
+document.body.appendChild(component());
