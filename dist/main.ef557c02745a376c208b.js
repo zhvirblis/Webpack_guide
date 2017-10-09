da6db9e4370e29862a5c94ec35320d291db6d107
zhvirblis <zhvirblis@github.com>
2017-10-09 14:21:23 +0300

    Caching: Extracting Boilerplate

---
+++ dist/main.ef557c02745a376c208b.js
@@ -0,0 +1,22 @@
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
+
+
+function component() {
+    var element = document.createElement('div');
+
+    return element;
+}
+
+document.body.appendChild(component());
+
+
+/***/ })
+],[1]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', 
+  entry: {
+    main: './src/index.js',
+    vendor: [
+      'lodash'
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HTMLWebpackPlugin({
       title: 'Caching'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
   ],
   output: {
