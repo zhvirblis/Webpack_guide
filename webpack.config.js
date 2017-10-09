e824feea12f4fd23bbae27d69b5259741b2a2e59
zhvirblis <zhvirblis@github.com>
2017-10-09 16:59:12 +0300

    Environment Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/polyfills.js',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-};
+module.exports = env => {
+    console.log('NODE_ENV: ', env.NODE_ENV) // 'local'
+    console.log('Production: ', env.production) // true
+
+    return {
+        entry: './src/polyfills.js',
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        }
+    };
+}
