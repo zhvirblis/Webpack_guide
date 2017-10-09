6610016cc139d549d0f9038131b0c50323ee7c36
zhvirblis <zhvirblis@github.com>
2017-10-09 16:33:49 +0300

    TypeScript: Basic Setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,21 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        polyfills: './src/polyfills.js',
-        index: './src/index.js'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+    entry: './src/polyfills.js',
     module: {
         rules: [
             {
-                test: require.resolve('./src/index.js'),
-                use: 'imports-loader?this=>window'
-            },
-            {
-                test: require.resolve('./src/globals.js'),
-                use: 'exports-loader?file,parse=helpers.parse'
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
             }
         ]
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            join: ['lodash', 'join']
-        })
-    ]
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
 };
